355a81e3666a66295a04d88e527c91e89a726004
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-04-08 19:59:28 -0300

    req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(123, 'victor')
+        encrypt_message('victor', 1.5)
+    assert encrypt_message('victor', 7) == 'rotciv'
+    assert encrypt_message('victor', -1) == 'rotciv'
+    assert encrypt_message('matheus', 6) == 's_uehtam'
+    assert encrypt_message('matheus', 5) == 'ehtam_su'
