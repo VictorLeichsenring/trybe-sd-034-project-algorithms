ffbd56aa353cf63711f09ac3cb4303c3ed918948
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-04-08 19:32:38 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    quantidade_online = 0
+
+    for horario in permanence_period:
+        if not (isinstance(horario[0], int) and isinstance(horario[1], int)):
+            return None
+
+        if horario[0] <= target_time <= horario[1]:
+            quantidade_online += 1
+
+    return quantidade_online
